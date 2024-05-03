2f94f72edce707d0b71496dfd948b73dd4a6b037
Dcerritos <139426429+sushitrash503@users.noreply.github.com>
2024-05-02 19:22:42 -0600

    Add files via upload

---
+++ src/main/java/cr24054/demo_cr24054/App.java
@@ -0,0 +1,18 @@
+package cr24054.demo_cr24054;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello Word";
+	}
+	
+    public static void main( String[] args )
+    {
+    	App hw = new App ();
+        System.out.println(hw.Hello() ) ;
+    }
+}
---
+++ src/test/java/cr24054/demo_cr24054/AppTest.java
@@ -0,0 +1,20 @@
+package cr24054.demo_cr24054;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello()
+    {
+        assertTrue( true );
+    }
+}
